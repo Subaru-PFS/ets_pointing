91c63d1a1b9c20da49c5cb55a6f3b4d030e91d94
Kiyoto Yabe <kiyoyabe@naoj.org>
2023-04-21 03:26:11 -1000

    fix a bug regarding flux of sky fibers

--- pfs_design_tool/pointing_utils/designutils.py
+++ pfs_design_tool/pointing_utils/designutils.py
@@ -200,15 +200,26 @@ def generate_pfs_design(
             # filter_names[i_fiber] = df_fluxstds["filterNames"][idx_fluxstd][0].tolist()
             if np.any(idx_sky):
                 cat_id[i_fiber] = df_sky["input_catalog_id"][idx_sky].values[0]
-                dict_of_flux_lists["psf_flux"][i_fiber] = np.array(
-                    [
-                        10**(-0.4*(df_sky["mag_thresh"][idx_sky].values[0] - 8.9))*1e+09,
-                        np.nan,
-                        np.nan,
-                        np.nan,
-                        np.nan,
-                    ]
-                )
+                try:
+                    dict_of_flux_lists["psf_flux"][i_fiber] = np.array(
+                        [
+                            10**(-0.4*(df_sky["mag_thresh"][idx_sky].values[0] - 8.9))*1e+09,
+                            np.nan,
+                            np.nan,
+                            np.nan,
+                            np.nan,
+                        ]
+                    )
+                except:
+                    dict_of_flux_lists["psf_flux"][i_fiber] = np.array(
+                        [
+                            np.nan,
+                            np.nan,
+                            np.nan,
+                            np.nan,
+                            np.nan,
+                        ]
+                    )
                 dict_of_flux_lists["psf_flux_error"][i_fiber] = np.array(
                     [
                         np.nan,
