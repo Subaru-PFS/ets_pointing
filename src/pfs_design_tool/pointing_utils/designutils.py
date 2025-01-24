650292f62fc49f9725999da5492e515b2939bb93
Kiyoto Yabe <kiyoyabe@naoj.org>
2025-01-23 18:02:25 -1000

    move the constraint for pm/parallax of guide stars to good_astrometry flag part

--- src/pfs_design_tool/pointing_utils/designutils.py
+++ src/pfs_design_tool/pointing_utils/designutils.py
@@ -540,19 +540,29 @@ def generate_guidestars_from_gaiadb(
         sqlWhere += f"AND source_id NOT EQUAL {gsId}"
 
     if good_astrometry is True:
-        astrometric_flag = "\n   AND astrometric_excess_noise_sig < 2.0"
+        astrometric_flag = f"""AND  {coldict['pmra']} IS NOT NULL   
+        AND {coldict['pmdec']} IS NOT NULL
+        AND {coldict['parallax']} IS NOT NULL
+        AND {coldict['parallax']} >= 0 
+        AND astrometric_excess_noise_sig < 2.0 
+        """
     else:
         astrometric_flag = ""
+
     query_string = f"""SELECT source_id,ra,dec,parallax,pmra,pmdec,ref_epoch,phot_g_mean_mag,bp_rp
     FROM gaia3
     WHERE q3c_radial_query(ra, dec, {ra_tel_deg}, {dec_tel_deg}, {search_radius})
-    AND {coldict['pmra']} IS NOT NULL
-    AND {coldict['pmdec']} IS NOT NULL
-    AND {coldict['parallax']} IS NOT NULL
-    AND {coldict['parallax']} >= 0 {astrometric_flag}
-    AND {coldict['mag']} BETWEEN {guidestar_mag_min} AND {guidestar_mag_max} {sqlWhere}
+    {astrometric_flag} AND {coldict['mag']} BETWEEN {guidestar_mag_min} AND {guidestar_mag_max} {sqlWhere}
     ;
     """
+    
+    #query_string = f"""SELECT source_id,ra,dec,parallax,pmra,pmdec,ref_epoch,phot_g_mean_mag,bp_rp
+    #FROM gaia3
+    #WHERE q3c_radial_query(ra, dec, {ra_tel_deg}, {dec_tel_deg}, {search_radius})
+    #AND {coldict['mag']} BETWEEN {guidestar_mag_min} AND {guidestar_mag_max} {sqlWhere}
+    #;
+    #"""
+    
     print(query_string)
     cur.execute(query_string)
 
